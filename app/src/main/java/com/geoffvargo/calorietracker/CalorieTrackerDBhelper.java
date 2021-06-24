a825e6e3d4f191851ffea55b18fb5e843189be2e
geoffvargo <geoff.vargo@stonybrook.edu>
2021-06-24 18:00:22 -0400

    Fully implemented SQL for the CalorieTrackerDBhelper class.

--- app/src/main/java/com/geoffvargo/calorietracker/CalorieTrackerDBhelper.java
+++ app/src/main/java/com/geoffvargo/calorietracker/CalorieTrackerDBhelper.java
@@ -2,6 +2,7 @@ package com.geoffvargo.calorietracker;
 
 import android.content.*;
 import android.database.sqlite.*;
+import android.util.*;
 
 import androidx.annotation.*;
 
@@ -9,17 +10,26 @@ class CalorieTrackerDBhelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "calorieTracker.db";
 	private static final int DATABASE_VERSION = 1;
 
+	// Database creation sql statement
+	private static final String CREATE_TABLE_ITEM = "CREATE TABLE item(_id INTEGER PRIMARY KEY AUTOINCREMENT, food_name TEXT NOT NULL, " +
+	                                                "servings FLOAT DEFAULT 1.0, carbs FLOAT DEFAULT 0.0, protein FLOAT DEFAULT 0.0, fat FLOAT DEFAULT 0.0);";
+
 	public CalorieTrackerDBhelper(@Nullable Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-
+		db.execSQL(CREATE_TABLE_ITEM);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(CalorieTrackerDBhelper.class.getName(),
+		      "Upgrading database from version " + oldVersion + " to "
+		      + newVersion + ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS item");
+		onCreate(db);
 
 	}
 }
