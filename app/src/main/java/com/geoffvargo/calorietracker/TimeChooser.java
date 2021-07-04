b22004433d2f5dd0ff7a323773c05f7ad99b4f05
geoffvargo <geoff.vargo@stonybrook.edu>
2021-07-04 01:28:13 -0400

    Code cleanup.

--- app/src/main/java/com/geoffvargo/calorietracker/TimeChooser.java
+++ app/src/main/java/com/geoffvargo/calorietracker/TimeChooser.java
@@ -14,30 +14,24 @@ import androidx.appcompat.app.*;
 import androidx.lifecycle.*;
 
 public class TimeChooser extends AppCompatDialogFragment implements TimePickerDialog.OnTimeSetListener {
+	long currtime;
 	private TimeViewModel tvm;
 	private Timestamp timestamp;
 	private Calendar c;
-	long currtime;
 
 	@Override
 	public @NotNull Dialog onCreateDialog(Bundle savedInstanceState) {
 		tvm = new ViewModelProvider(requireActivity()).get(TimeViewModel.class);
-		// Use the current time as the default values for the picker
-		c      = Calendar.getInstance();
-		int            hour   = c.get(Calendar.HOUR_OF_DAY);
-		int            minute = c.get(Calendar.MINUTE);
-//		c.setTimeInMillis();
-		currtime = c.getTimeInMillis();
-//		timestamp = new Timestamp(c.getTimeInMillis());
 
-		// Create a new instance of TimePickerDialog and return it
+		c = Calendar.getInstance();
+		int hour   = c.get(Calendar.HOUR_OF_DAY);
+		int minute = c.get(Calendar.MINUTE);
+		currtime = c.getTimeInMillis();
+
 		return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
 	}
 
-
-
 	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-		// Do something with the time chosen by the user
 		c.set(Calendar.HOUR_OF_DAY, hourOfDay);
 		c.set(Calendar.MINUTE, minute);
 		timestamp = new Timestamp(c.getTimeInMillis());
