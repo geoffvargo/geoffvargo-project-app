8cf28e7360f4e2261f71d9422f26dca7934b1b22
geoffvargo <geoff.vargo@stonybrook.edu>
2021-07-05 14:54:50 -0400

    Started implementing SettingsActivity.

--- app/src/main/java/com/geoffvargo/calorietracker/SettingsActivity.java
+++ app/src/main/java/com/geoffvargo/calorietracker/SettingsActivity.java
@@ -1,35 +1,14 @@
 package com.geoffvargo.calorietracker;
 
 import android.os.*;
-import android.view.*;
 
 import androidx.appcompat.app.*;
-import androidx.preference.*;
 
 public class SettingsActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.settings_activity);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager()
-					.beginTransaction()
-					.replace(R.id.settings, new SettingsFragment())
-					.commit();
-		}
-		ActionBar actionBar = getSupportActionBar();
-		View      sortBy    = findViewById(R.id.sortByLPREF);
-//		sortBy.seton
-		if (actionBar != null) {
-			actionBar.setDisplayHomeAsUpEnabled(true);
-		}
-	}
-
-	public static class SettingsFragment extends PreferenceFragmentCompat {
-		@Override
-		public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-			setPreferencesFromResource(R.xml.root_preferences, rootKey);
-		}
+		setContentView(R.layout.activity_settings);
 	}
 }
