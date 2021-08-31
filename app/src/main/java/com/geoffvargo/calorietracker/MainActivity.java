5f63524fd53ee64a1acf51413e4c1c725651a854
geoffvargo <geoff.vargo@stonybrook.edu>
2021-08-31 03:32:08 -0400

    Continuing setup of bottomNavigation.

--- app/src/main/java/com/geoffvargo/calorietracker/MainActivity.java
+++ app/src/main/java/com/geoffvargo/calorietracker/MainActivity.java
@@ -3,22 +3,24 @@
 package com.geoffvargo.calorietracker;
 
 import android.os.*;
-import android.util.*;
 import android.view.*;
 import android.widget.*;
 
+import com.geoffvargo.calorietracker.databinding.*;
 import com.google.android.material.bottomnavigation.*;
-
-import org.jetbrains.annotations.*;
 
 import java.util.*;
 
-import androidx.annotation.*;
 import androidx.appcompat.app.*;
 import androidx.core.content.*;
+import androidx.navigation.*;
+import androidx.navigation.ui.*;
 import androidx.recyclerview.widget.*;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+//public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+	private ActivityMainBinding binding;
+
 	private RecyclerView foodItemView;
 	private ArrayList<FoodItem> foodItems = new ArrayList<>();
 	private FoodItemAdapter adapter;
@@ -28,10 +30,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 	private String sortBy;
 	private String sortOrder;
 	private BottomNavigationView bottomNavView;
+	private NavController navController;
 
 	private Home home = new Home();
 	private Today today = new Today();
 	private Meals meals = new Meals();
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,18 +47,25 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+		binding = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+
+//		setContentView(R.layout.activity_main);
 		Window w = getWindow();
 		w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 		w.setNavigationBarColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
 
+
+
 //		toolBarLayout.setTitle(getTitle());
 
 		bottomNavView = findViewById(R.id.bottomNavVIEW);
-		bottomNavView.setSelectedItemId(R.id.homeNavITEM);
-		bottomNavView.setOnNavigationItemSelectedListener(this);
+//		bottomNavView.setSelectedItemId(R.id.homeNavITEM);
+//		bottomNavView.setOnNavigationItemSelectedListener(this);
+		NavController navController = Navigation.findNavController(this, R.id.navHost);
 
-//		NavigationUI.setupWithNavController(bottomNavView., navController);
+		NavigationUI.setupWithNavController(binding.bottomNavVIEW, navController);
 	}
 
 	@Override
@@ -69,22 +81,22 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 		this.foodItems = foodItems;
 	}
 
-	@Override
-	public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
-		int itemId = item.getItemId();
-		if (itemId == R.id.homeNavITEM) {
-			getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, home).commit();
-			Log.d("bottomNav", "home");
-			return true;
-		} else if (itemId == R.id.todayNavITEM) {
-			getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, today).commit();
-			Log.d("bottomNav", "today");
-			return true;
-		} else if (itemId == R.id.mealsNavITEM) {
-			getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, meals).commit();
-			Log.d("bottomNav", "meals");
-			return true;
-		}
-		return false;
-	}
+//	@Override
+//	public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+//		int itemId = item.getItemId();
+//		if (itemId == R.id.homeNavITEM) {
+//			getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, home).commit();
+//			Log.d("bottomNav", "home");
+//			return true;
+//		} else if (itemId == R.id.todayNavITEM) {
+//			getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, today).commit();
+//			Log.d("bottomNav", "today");
+//			return true;
+//		} else if (itemId == R.id.mealsNavITEM) {
+//			getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, meals).commit();
+//			Log.d("bottomNav", "meals");
+//			return true;
+//		}
+//		return false;
+//	}
 }
